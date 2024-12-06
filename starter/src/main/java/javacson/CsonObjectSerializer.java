0ad667b0cacb7f8a65caee3207fe9729bad05f54
RPadhi678 <116604147+RPadhi678@users.noreply.github.com>
2024-12-06 15:09:44 -0800

    Adding my starter code

---
+++ starter/src/main/java/javacson/CsonObjectSerializer.java
@@ -0,0 +1,14 @@
+package javacson;
+import java.util.List;
+
+class CsonObjectSerializer {
+    public String serialize(List<Object> objects) {
+        // TODO
+        return null;
+    }
+
+    public String serialize(Object object) {
+        // TODO
+        return null;
+    }
+}
---
+++ starter/src/test/java/javacson/StudentTest.java
@@ -0,0 +1,18 @@
+package javacson;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import org.junit.jupiter.api.Test;
+
+public class StudentTest {
+    @Test
+    public void myTest0() {
+        // TODO
+        assert false;
+    }
+
+    @Test
+    public void myTest1() {
+        assert false;
+    }
+}
